53da2103e70f453a099737cbb63c6f88a489a396
uzgurdev <uzguruzb@gmail.com>
2024-02-16 14:30:15 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import { Form } from "./components";
-import Courses from "./components/courses";
 import { UseReduce } from "./hooks";
 import TableElm from "./components/table";
+import NavItem from "./components/navigationBar";
+import Barmenu from "./components/barmenu";
+import SearchBar from "./components/searchbar";
+import Footer from "./components/footer";
 
 function App() {
   const { state, dispatch } = UseReduce();
@@ -11,39 +12,11 @@ function App() {
 
   return (
     <div className="App flex items-center gap-[20px]">
+      <Barmenu />
       <TableElm elements={state} />
-    </div>
-  );
-}
-
-export default App;
-import { useState } from "react";
-import "./App.css";
-import { Form } from "./components";
-import Courses from "./components/courses";
-import Barmenu from "./components/barmenu";
-import NavItem from "./components/navigationBar";
-import SearchBar from "./components/searchbar";
-import Footer from "./components/footer";
-
-function App() {
-  const [courses, setCourses] = useState([
-    "react",
-    "vue",
-    "angular",
-    "java",
-    "java backend",
-  ]);
-  const [specializeCourse, setSpecializeCourse] = useState([]);
-
-  return (
-    <div className="App flex items-center gap-[20px]">
-       <Barmenu />
-      <NavItem  />
+      <NavItem />
       <SearchBar />
-      <Form />
-      <Courses />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
